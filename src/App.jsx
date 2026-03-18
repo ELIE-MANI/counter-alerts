99d308a91e0c170eec294795bd394288e0aad627
ELIE-MANI <eliemaniraguha@gmail.com>
2026-03-18 12:51:14 +0200

    Build the UI using tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ import { useEffect, useState } from "react"
 export default function App() {
   const [count, setCount]= useState(0);
   const [message, setMessage] = useState("");
-  const [hasAlerted, setHasAlerted] = useState(false)
+  
 
-
-
-
-  useEffect(() => {
+   useEffect(() => {
     if(count === 10){
       alert("🎉 You reached 10!")
+    }
+    if (count === -5){
+      alert("⚠️ Too low!")
     }
     if (count === 0){
      setMessage("Start Counting...");
@@ -27,13 +27,23 @@ export default function App() {
 
 
   return (
-    <div className=''>
-    <h1>{count}</h1>
-    <button onClick={() => setCount(count - 1)}>-</button>
-    <button onClick={() => setCount(0)}>Reset</button>
-    <button onClick={() => setCount(count + 1)}>+</button>
-    <p>{message}</p>
-          
+    <div className='min-h-screen flex items-center justify-center bg-gray-100'>
+    <div className="bg-white p-6 rounded-2xl shadow-xl text-center w-80 space-y-6">
+    <h1 className="text-5xl font-bold">
+      {count}</h1>
+    <div className="flex justify-center gap-4">
+    <button className="px-4 py-2 bg-red-500 text-white rounded"
+    onClick={() => setCount(count - 1)}
+    >-</button>
+    <button className="px-4 py-2 bg-gray-500 text-white rounded" 
+    onClick={() => setCount(0)}
+    >Reset</button>
+    <button className="px-4 py-2 bg-green-500 text-white rounded"
+     onClick={() => setCount(count + 1)}
+     >+</button>
+    </div>
+    <p className="text-gray-600">{message}</p>
+    </div>  
     </div>
   )
 }
