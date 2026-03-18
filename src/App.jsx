4f3f23aa171ac092bf366eaf0e9289b7dac15bcd
ELIE-MANI <eliemaniraguha@gmail.com>
2026-03-18 12:35:00 +0200

    feat: used useEffect to show message whe count is updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
 export default function App() {
   const [count, setCount]= useState(0);
+  const [message, setMessage] = useState("");
+  const [hasAlerted, setHasAlerted] = useState(false)
 
 
 
+
+  useEffect(() => {
+    if(count === 10){
+      alert("🎉 You reached 10!")
+    }
+    if (count === 0){
+     setMessage("Start Counting...");
+    }
+    else if (count > 0){
+      setMessage("Positive Number");
+    }
+    else{
+      setMessage("Negative Number");
+    }
+
+  }, [count])
 
 
   return (
@@ -14,6 +32,7 @@ export default function App() {
     <button onClick={() => setCount(count - 1)}>-</button>
     <button onClick={() => setCount(0)}>Reset</button>
     <button onClick={() => setCount(count + 1)}>+</button>
+    <p>{message}</p>
           
     </div>
   )
