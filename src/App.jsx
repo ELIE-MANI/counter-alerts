1bd1cfc6ffd1ad2f13ac4771c19c4adcfc532b95
ELIE-MANI <eliemaniraguha@gmail.com>
2026-03-18 11:30:46 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+
+
+export default function App() {
+  const [count, setCount]= useState(0);
+
+
+
+
+
+  return (
+    <div className=''>
+    <h1>0</h1>
+    <button>-</button>
+    <button>+</button>
+          
+    </div>
+  )
+}
